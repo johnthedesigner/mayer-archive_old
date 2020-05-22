6f946a7105da000fae096833668cf73a20f76e8a
johnthedesigner <john@johnthedesigner.com>
2020-05-22 17:28:40 -0400

    Load images from cloudinary

--- pages/catalog/images/[image_id].js
+++ pages/catalog/images/[image_id].js
@@ -2,10 +2,12 @@ import Head from 'next/head'
 import Link from 'next/link'
 import _ from 'lodash'
 
-import { getImage, getImages } from '../../../lib/api'
+import { getImage, getImages, getEnvelope } from '../../../lib/api'
 
-const ImageDetail = (image) => {
-  let envelope_id = image.image_id.split('-')[0]
+const ImageDetail = ({envelope, image}) => {
+  const {image_id, caption_on_envelope} = image
+  const {date_developed, envelope_id, envelope_notes} = envelope
+  const exposure_number = image_id.split('-')[1]
 
   return (
     <div className="container">
@@ -14,6 +16,7 @@ const ImageDetail = (image) => {
         </h1>
 
         <h3>{image.image_id}</h3>
+        <img src={`https://res.cloudinary.com/dkpugzzu5/image/upload/v1590179543/mayer-archive/${envelope_id}_${date_developed}_${exposure_number}.jpg`}/>
         <h4>
           <b>Envelope: </b>
           <Link href={`/catalog/envelopes/${envelope_id}`}>
@@ -44,9 +47,11 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
+  const envelope_id = params.image_id.split('-')[0]
   const image = (await getImage(params.image_id)) || []
+  const envelope = (await getEnvelope(envelope_id)) || []
   return {
-    props: { ...image },
+    props: { image, envelope }
   }
 }
 
